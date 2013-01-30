5f0de2589d2699164196a533d3d2e59a11dfba78
Ivan A. Melnikov <imelnikov@griddynamics.com>
2013-01-30 11:15:25 +0400

    Update check_code.sh
    Change-Id: I2d866fed3d130eb9b77bd75c2f6ff1e89e8eabe5

--- check_code.sh
+++ check_code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
-pep8 --ignore=W391,E201 altai_api tests
+pep8 --repeat --ignore=W391,E201 altai_api tests
+rm -f .coverage
 nosetests --with-coverage --cover-package=altai_api
 pylint  -r n tests altai_api
 
